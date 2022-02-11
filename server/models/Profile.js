4c298b139b70a7e2a9049b28f23fa2b73f00278c
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-02-11 17:55:49 -0500

    added models to resolver files, filled out profile, toVisit, and visited models

---
+++ server/models/Profile.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const profileSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  toVisit: [
+    {
+        type: Schema.Types.ObjectId, // refers to the _id object type
+        ref: "toVisit" // refers to the collection called toVisit in toVisit.js
+      }
+  ],
+  Visited: [
+    {
+        type: Schema.Types.ObjectId, // refers to the _id object type
+        ref: "Visted" // refers to the collection called Visted in Visted.js
+      }
+  ],
+});
+
+// set up pre-save middleware to create password
+profileSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+});
+
+// compare the incoming password with the hashed password
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+const Profile = model('Profile', profileSchema);
+
+module.exports = Profile;
---
+++ server/models/Visited.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+
+const VisitedSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  pricePoint: {
+    type: String,
+    trim: true,
+    // required: true,
+  },
+  comment: {
+    type: String,
+    trim: true,
+    minlength: 1,
+    maxlength: 280,
+    // required: true,
+  },
+  dateVisted: {
+    type: String,
+    trim: true,
+    // required: true,
+  },
+  rating: {
+    type: String,
+    trim: true,
+    // required: true,
+  },
+  picture: {
+    type: String,
+    trim: true,
+    // required: true,
+  },
+});
+
+const Visited = model("Visited", VisitedSchema);
+
+module.exports = Visited;
