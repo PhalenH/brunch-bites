b3a6dc6584070d54614b24c05fe24d497343669d
Kieran Harris <kharris19063@gmail.com>
2022-02-22 18:45:51 -0500

    Adding and troubleshooting ToVisit componenet

--- client/src/components/toVisitList/index.js
+++ client/src/components/toVisitList/index.js
@@ -7,33 +7,35 @@ const ToVisitList = ({ toVisitResults }) => {
   }
 
   return (
-
-    <li class="p-2">
-<div class="scene scene--card">
-  <div class="card">
-    <div class="card__face card__face--front">
-      <div class="card-body">
-        <button class="btn btn-dark"> <i class="bi bi-check-square"></i> Visited </button>
-        <button class="btn btn-dark"> <i class="bi bi-dash-square"></i> Let's Not </button>
-        <button class="flip-to-back btn btn-dark"> <i class="bi bi-arrow-clockwise"></i> Flip for more info</button>
+<div>
+{toVisitResults &&
+          toVisitResults.map((toVisit) => (
+    <li className="p-2">
+<div className="scene scene--card">
+  <div className="card">
+    <div className="card__face card__face--front">
+      <div className="card-body">
+        <button className="btn btn-dark"> <i className="bi bi-check-square"></i> Visited </button>
+        <button className="btn btn-dark"> <i className="bi bi-dash-square"></i> Let's Not </button>
+        <button className="flip-to-back btn btn-dark"> <i className="bi bi-arrow-clockwise"></i> Flip for more info</button>
       </div>
       <img src="./placeholder.png" alt="temp" style="width: 50%; height: 50%"/>
-      <div class="card-body">
-        <h5 class="card-title">{toVisit.name}</h5>
+      <div className="card-body">
+        <h5 className="card-title">{toVisit.name}</h5>
       </div>
     </div>
-    <div class="card__face card__face--back">
-      <div class="card-body">
-        <button class="flip-to-front btn btn-dark"> <i class="bi bi-arrow-clockwise"></i> Go back</button>
+    <div className="card__face card__face--back">
+      <div className="card-body">
+        <button className="flip-to-front btn btn-dark"> <i className="bi bi-arrow-clockwise"></i> Go back</button>
       </div>
-      <ul class="list-group list-group-flush">
-        <li class="list-group-item">Rating: {toVisit.rating}</li>
-        <li class="list-group-item">Price: {toVisit.price}</li>
-        <li class="list-group-item">Address: {toVisit.location.address1} {toVisit.location.city} {toVisit.location.zip_code} {toVisit.location.state}</li>
-        <li class="list-group-item">
-          <div class="input-group">
-            <textarea class="form-control" aria-label="With textarea" placeholder="Why are we visiting?"> {toVisit.comment} </textarea>
-            <button class="btn btn-outline-secondary" type="button">Save</button>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">Rating: {toVisit.rating}</li>
+        <li className="list-group-item">Price: {toVisit.price}</li>
+        <li className="list-group-item">Address: {toVisit.location.address1} {toVisit.location.city} {toVisit.location.zip_code} {toVisit.location.state}</li>
+        <li className="list-group-item">
+          <div className="input-group">
+            <textarea className="form-control" aria-label="With textarea" placeholder="Why are we visiting?"> {toVisit.comment} </textarea>
+            <button className="btn btn-outline-secondary" type="button">Save</button>
           </div>
         </li>
       </ul>
@@ -41,7 +43,8 @@ const ToVisitList = ({ toVisitResults }) => {
   </div>
 </div>
 </li>
-
+))}
+</div>
   );
 };
 
