3a4b9ce3de6da45d0e55275f024bad724bd15298
Kieran Harris <kharris19063@gmail.com>
2022-02-22 09:32:56 -0500

    Adding template to index.js of toVisitList

--- client/src/components/toVisitList/index.js
+++ client/src/components/toVisitList/index.js
@@ -7,34 +7,43 @@ const ToVisitList = ({ toVisitResults }) => {
   }
 
   return (
-    <div>
-      <h3 className="">{}</h3>
-      <div className="">
-        {toVisitResults &&
-          toVisitResults.map((toVisit) => (
-            <div key={toVisit._id} className="">
-              <div className="">
-                <h4 className="">
-                  {toVisit.name} <br />
-                  <ul>
-                    <li>{toVisit.location.address1}</li>
-                    <li>{toVisit.location.city}</li>
-                    <li>{toVisit.location.zip_code}</li>
-                    <li>{toVisit.location.state}</li>
-                    <li>{toVisit.price}</li>
-                    <li>{toVisit.rating}</li>
-                    <li>{toVisit.comment}</li>
-                  </ul>
-                </h4>
-                <a className="" href={`${toVisit.url}`}>
-                  View the yelp url.
-                </a>
-              </div>
-            </div>
-          ))}
+
+    <li class="p-2">
+<div class="scene scene--card">
+  <div class="card">
+    <div class="card__face card__face--front">
+      <div class="card-body">
+        <button class="btn btn-dark"> <i class="bi bi-check-square"></i> Visited </button>
+        <button class="btn btn-dark"> <i class="bi bi-dash-square"></i> Let's Not </button>
+        <button class="flip-to-back btn btn-dark"> <i class="bi bi-arrow-clockwise"></i> Flip for more info</button>
+      </div>
+      <img src="./placeholder.png" alt="temp" style="width: 50%; height: 50%"/>
+      <div class="card-body">
+        <h5 class="card-title">{toVisit.name}</h5>
       </div>
     </div>
+    <div class="card__face card__face--back">
+      <div class="card-body">
+        <button class="flip-to-front btn btn-dark"> <i class="bi bi-arrow-clockwise"></i> Go back</button>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Rating: {toVisit.rating}</li>
+        <li class="list-group-item">Price: {toVisit.price}</li>
+        <li class="list-group-item">Address: {toVisit.location.address1} {toVisit.location.city} {toVisit.location.zip_code} {toVisit.location.state}</li>
+        <li class="list-group-item">
+          <div class="input-group">
+            <textarea class="form-control" aria-label="With textarea" placeholder="Why are we visiting?"> {toVisit.comment} </textarea>
+            <button class="btn btn-outline-secondary" type="button">Save</button>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+</li>
+
   );
 };
 
 export default ToVisitList;
+
