3fb89af4a42da09438c5ff84589a29a1f9de650c
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2022-02-21 21:12:53 -0500

    changed searchbars.js to index.js, removed searchResults from results page

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -3,27 +3,20 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 
 import Header from "../components/Header";
-import SearchBar from "../components/SearchBar/SearchBar";
-import SearchResults from "../components/SearchResults";
-import { QUERY_BRUNCH_SPOT_LIST } from "../utils/queries";
+import SearchBar from "../components/SearchBar";
+// import SearchResults from "../components/SearchResults";
+// import { QUERY_BRUNCH_SPOT_LIST } from "../utils/queries";
 
 const Results = () => {
-  const { loading, data } = useQuery(QUERY_BRUNCH_SPOT_LIST);
-  const brunchSpotList = data?.brunchSpotList || [];
-  console.log(brunchSpotList);
+
   return (
     <main>
       <div className="">
         <Header />
         <div className="">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
             <div>
             <SearchBar placeholder="Enter info" />
-            <SearchResults results={brunchSpotList} />
             </div>
-          )}
         </div>
       </div>
     </main>
