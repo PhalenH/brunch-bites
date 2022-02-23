d0ee2d2a7167d744d8340f4235ef92a50ea8dd9d
Kieran Harris <kharris19063@gmail.com>
2022-02-23 09:08:01 -0500

    Rearranged restaurant lists for ToVist and Visted Sections, added styles

--- client/src/components/VisitedList/index.js
+++ client/src/components/VisitedList/index.js
@@ -1,39 +1,116 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const VistedList = ({ visitedResults }) => {
-  if (!visitedResults) {
-    return <h3>You aint been nowhere</h3>;
+const styles = {
+  card: {
+    width:"18rem",
   }
+}
+
+
+const VistedList = ({ visitedResults }) => {
+  // if (!visitedResults) {
+  //   return <h3>You aint been nowhere</h3>;
+  // }
 
   return (
     <div>
+      {/* {toVisitResults &&
+          toVisitResults.map((toVisit) => ( */}
+
+      <div>
+        <button type="button" className="btn btn-dark" id="slideLeft"><i class="bi bi-arrow-left-circle"></i>Left</button>
+        <button type="button" className="btn btn-dark" id="slideRight"><i class="bi bi-arrow-right-circle"></i>Right</button>
+      </div>
+
       <h3 className="">{}</h3>
       <div className="">
-        {visitedResults &&
-          visitedResults.map((visited) => (
-            <div key={visited._id} className="">
-              <div className="">
-                <h4 className="">
-                  {visited.name} <br />
-                  <ul>
-                    <li>{visited.location.address1}</li>
-                    <li>{visited.location.city}</li>
-                    <li>{visited.location.zip_code}</li>
-                    <li>{visited.location.state}</li>
-                    <li>{visited.price}</li>
-                    <li>{visited.myRating}</li>
-                    <li>{visited.comment}</li>
-                    <li>{visited.dateVisited}</li>
-                  </ul>
-                </h4>
-                <a className="" href={`${visited.url}`}>
-                  View the yelp url.
-                </a>
+        {/* {visitedResults &&
+          visitedResults.map((visited) => ( */}
+      <div className="scrollmenu" id="container">
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 1</h5>
               </div>
-            </div>
-          ))}
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 2</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 3</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 4</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 5</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 6</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
       </div>
+
+          {/* ))} */}
+      </div>
+      {/* ))} */}
     </div>
   );
 };
--- client/src/components/toVisitList/index.js
+++ client/src/components/toVisitList/index.js
@@ -1,52 +1,111 @@
 import React from "react";
+import "./ToVisit.css";
 import { Link } from "react-router-dom";
 
-const ToVisitList = ({ toVisitResults }) => {
-  if (!toVisitResults) {
-    return <h3>Add a place to view list</h3>;
+const styles = {
+  card: {
+    width:"18rem",
   }
+}
+
+const ToVisitList = ({ toVisitResults }) => {
+  // if (!toVisitResults) {
+  //   return <h3>Add a place to view list</h3>;
+  // }
+  
 
   return (
 <div>
-{toVisitResults &&
-          toVisitResults.map((toVisit) => (
-    <li className="p-2">
-<div className="scene scene--card">
-  <div className="card">
-    <div className="card__face card__face--front">
-      <div className="card-body">
-        <button className="btn btn-dark"> <i className="bi bi-check-square"></i> Visited </button>
-        <button className="btn btn-dark"> <i className="bi bi-dash-square"></i> Let's Not </button>
-        <button className="flip-to-back btn btn-dark"> <i className="bi bi-arrow-clockwise"></i> Flip for more info</button>
-      </div>
-      <img src="./placeholder.png" alt="temp" style="width: 50%; height: 50%"/>
-      <div className="card-body">
-        <h5 className="card-title">{toVisit.name}</h5>
-      </div>
-    </div>
-    <div className="card__face card__face--back">
-      <div className="card-body">
-        <button className="flip-to-front btn btn-dark"> <i className="bi bi-arrow-clockwise"></i> Go back</button>
-      </div>
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">Rating: {toVisit.rating}</li>
-        <li className="list-group-item">Price: {toVisit.price}</li>
-        <li className="list-group-item">Address: {toVisit.location.address1} {toVisit.location.city} {toVisit.location.zip_code} {toVisit.location.state}</li>
-        <li className="list-group-item">
-          <div className="input-group">
-            <textarea className="form-control" aria-label="With textarea" placeholder="Why are we visiting?"> {toVisit.comment} </textarea>
-            <button className="btn btn-outline-secondary" type="button">Save</button>
+{/* {toVisitResults &&
+          toVisitResults.map((toVisit) => ( */}
+        <div>
+          <button type="button" className="btn btn-dark" id="slideLeft"><i class="bi bi-arrow-left-circle"></i>Left</button>
+          <button type="button" className="btn btn-dark" id="slideRight"><i class="bi bi-arrow-right-circle"></i>Right</button>
+        </div>
+      <div className="scrollmenu" id="container">
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 1</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
           </div>
-        </li>
-      </ul>
-    </div>
-  </div>
-</div>
-</li>
-))}
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 2</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 3</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 4</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 5</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+        <div className="col-4">
+          <div className="card" style={styles.card}>
+            <button type="button" className="btn btn-dark"><i className="bi bi-check-circle"></i> Visited </button>
+            <button type="button" className="btn btn-dark"><i className="bi bi-x-circle"></i> Remove </button>
+            <img className="card-img-top" src="./placeholder.png" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">Restaurant Name 6</h5>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Rating: **** Price: $$</li>
+              </ul>
+          </div>
+        </div>
+      </div>
+
+{/* ))} */}
 </div>
   );
 };
 
 export default ToVisitList;
-
